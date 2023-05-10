e42cf4be95a2022ec855ee228437916eb0cb7981
laurenz <i22032@hb.dhbw-stuttgart.de>
2023-05-10 23:57:27 +0200

    filling of DB done, last commit before changes in main code

--- src/main/java/DataBase/DBinteractions.java
+++ src/main/java/DataBase/DBinteractions.java
@@ -3,6 +3,8 @@ package DataBase;
 import java.io.File;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBinteractions {
     private Connection conn;
@@ -24,8 +26,9 @@ public class DBinteractions {
 
     private DBinteractions() throws SQLException {
         try {
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/LegoBattlesMapGenerator"
-                    , DBhelper.readLine("C:\\Users\\laure\\Desktop\\user.txt"), DBhelper.readLine("C:\\Users\\laure\\Desktop\\password.txt"));
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/LegoBattlesMapGenerator",
+                    DBhelper.readLine("C:\\Users\\laure\\Desktop\\user.txt"),
+                    DBhelper.readLine("C:\\Users\\laure\\Desktop\\password.txt"));
             conn.setSchema("legoBattlesMapGeneratorSchema");
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -44,21 +47,30 @@ public class DBinteractions {
             if (file.isFile() && file.getName().toLowerCase().endsWith(".png")) {
                 String imageUrl = file.getAbsolutePath();
                 PreparedStatement stmt = conn.prepareStatement(
-                        "INSERT INTO tile_content (filepath) VALUES (?)");
+                        "INSERT INTO tile_content (filepath) VALUES (?) ON CONFLICT (filepath) DO NOTHING");
                 stmt.setString(1, imageUrl);
                 stmt.executeUpdate();
             }
         }
     }
 
-    public void putRuleIntoDB(int this_tile, int next_to, int that_tile, boolean can_it_be) throws SQLException {
+    public List<Integer> putRuleIntoDB(int this_tile, int next_to, int that_tile, boolean can_it_be) throws SQLException {
+        List<Integer> createdRuleIds = new ArrayList<>();
+
         PreparedStatement stmt = conn.prepareStatement(
-                "INSERT INTO rule (this_tile, next_to, that_tile , allowed) VALUES (?, ?, ?, ?)");
+                "INSERT INTO rule (this_tile, next_to, that_tile , can_it_be) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         stmt.setInt(1, this_tile);
         stmt.setInt(2, next_to);
         stmt.setInt(3, that_tile);
         stmt.setBoolean(4, can_it_be);
         stmt.executeUpdate();
+
+        // Get the generated rule ID(s)
+        ResultSet generatedKeys = stmt.getGeneratedKeys();
+        while (generatedKeys.next()) {
+            int ruleId = generatedKeys.getInt(1);
+            createdRuleIds.add(ruleId);
+        }
 
         // Get the reverse_id of next_to from the direction table
         PreparedStatement directionStmt = conn.prepareStatement(
@@ -70,23 +82,33 @@ public class DBinteractions {
 
             // Insert the reversed rule into the rule table
             PreparedStatement reversedStmt = conn.prepareStatement(
-                    "INSERT INTO rule (this_tile, next_to, that_tile, allowed) VALUES (?, ?, ?, ?)");
+                    "INSERT INTO rule (this_tile, next_to, that_tile, can_it_be) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             reversedStmt.setInt(1, that_tile);
             reversedStmt.setInt(2, reverseNextTo);
             reversedStmt.setInt(3, this_tile);
             reversedStmt.setBoolean(4, can_it_be);
             reversedStmt.executeUpdate();
+
+            // Get the generated rule ID(s)
+            generatedKeys = reversedStmt.getGeneratedKeys();
+            while (generatedKeys.next()) {
+                int reversedRuleId = generatedKeys.getInt(1);
+                createdRuleIds.add(reversedRuleId);
+            }
         }
+
+        return createdRuleIds;
     }
 
-    public RuleCreationTO getNewRule() throws SQLException {
+
+    public RuleCreationTO getNewRule() throws SQLException, NoMoreRulesException {
         // Query the database for tile pairs without rules
         PreparedStatement stmt = conn.prepareStatement(
                 "SELECT this_tile.id AS this_tile_id, that_tile.id AS that_tile_id, direction.id AS direction_id, this_tile.filepath AS this_tile_filepath, that_tile.filepath AS that_tile_filepath " +
                         "FROM tile_content AS this_tile, tile_content AS that_tile, direction " +
                         "WHERE NOT EXISTS (SELECT * FROM rule WHERE this_tile = this_tile.id AND next_to = direction.id AND that_tile = that_tile.id) " +
                         "AND NOT EXISTS (SELECT * FROM rule WHERE this_tile = that_tile.id AND next_to = direction.reverse_id AND that_tile = this_tile.id) " +
-                        "ORDER BY random() " +
+                        "ORDER BY this_tile_id ASC, that_tile_id ASC, direction_id ASC " +
                         "LIMIT 1"
         );
         ResultSet rs = stmt.executeQuery();
@@ -100,9 +122,22 @@ public class DBinteractions {
             String thatTileFilepath = rs.getString("that_tile_filepath");
             return new RuleCreationTO(thisTileId, directionId, thatTileId, thisTileFilepath, thatTileFilepath);
         } else {
-            throw new RuntimeException("No Tiles found that don't have rules");
+            throw new NoMoreRulesException();
         }
     }
 
+    public void revertRule(List<Integer> lastRule) {
+        for (Integer ruleId:lastRule) {
+            try {
+                PreparedStatement statement = conn.prepareStatement(
+                        "DELETE FROM rule WHERE id = ?"
+                );
+                statement.setInt(1, ruleId);
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
--- src/main/java/DataBase/RuleCreationGUI.java
+++ src/main/java/DataBase/RuleCreationGUI.java
@@ -1,98 +1,190 @@
 package DataBase;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class RuleCreationGUI extends JFrame implements ActionListener {
-    private JLabel thisTileLabel, thatTileLabel;
-    private JButton fitsButton, doesnFitsButton;
+
+    private JButton fitsButton;
+    private JButton doesnFitsButton;
+    private JButton revertLastRuleButton;
+    private Square squareTopLeft;
+    private Square squareTopRight;
+    private Square squareDownLeft;
+    private Square squareDownRight;
     private RuleCreationTO currentRule;
-    private DBinteractions dbInteractions;
+    private List<Integer> lastRule;
 
-    public RuleCreationGUI() throws SQLException {
-        super("Create Rule");
-        dbInteractions =  DBinteractions.getInstance();
-        currentRule = dbInteractions.getNewRule();
-        displayRule();
-        setSize(400, 400);
+    private DBinteractions dbInteractions = DBinteractions.getInstance();
+
+    public RuleCreationGUI() {
+        super("RuleCreationGUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+        setSize(500, 500);
 
-    private void displayRule() {
         JPanel mainPanel = new JPanel(new BorderLayout());
 
-        JPanel tilesPanel = new JPanel(new FlowLayout());
-        thisTileLabel = new JLabel(new ImageIcon(currentRule.this_TileFilepath()));
-        thatTileLabel = new JLabel(new ImageIcon(currentRule.that_TileFilepath()));
-        int nextTo = currentRule.next_to();
-        switch (nextTo) {
-            case 1 -> tilesPanel.add(thisTileLabel, BorderLayout.NORTH);
-            case 2 -> tilesPanel.add(thisTileLabel, BorderLayout.SOUTH);
-            case 3 -> tilesPanel.add(thisTileLabel, BorderLayout.EAST);
-            case 4 -> tilesPanel.add(thisTileLabel, BorderLayout.WEST);
-            case 5 -> {
-                GridBagConstraints c = new GridBagConstraints();
-                c.gridx = 0;
-                c.gridy = 0;
-                tilesPanel.add(thisTileLabel, c);
-            }
-            case 6 -> {
-                GridBagConstraints c = new GridBagConstraints();
-                c.gridx = 1;
-                c.gridy = 0;
-                tilesPanel.add(thisTileLabel, c);
-            }
-            case 7 -> {
-                GridBagConstraints c = new GridBagConstraints();
-                c.gridx = 0;
-                c.gridy = 1;
-                tilesPanel.add(thisTileLabel, c);
-            }
-            case 8 -> {
-                GridBagConstraints c = new GridBagConstraints();
-                c.gridx = 1;
-                c.gridy = 1;
-                tilesPanel.add(thisTileLabel, c);
-            }
-        }
-        tilesPanel.add(thatTileLabel);
+        JPanel tilesPanel = new JPanel(new GridLayout(2, 2));
+        squareTopLeft = new Square();
+        squareTopRight = new Square();
+        squareDownLeft = new Square();
+        squareDownRight = new Square();
+        tilesPanel.add(squareTopLeft);
+        tilesPanel.add(squareTopRight);
+        tilesPanel.add(squareDownLeft);
+        tilesPanel.add(squareDownRight);
 
         JPanel buttonPanel = new JPanel(new FlowLayout());
         fitsButton = new JButton("Fits");
         fitsButton.addActionListener(this);
         doesnFitsButton = new JButton("Doesn't fit");
         doesnFitsButton.addActionListener(this);
+        revertLastRuleButton = new JButton("revert");
+        revertLastRuleButton.addActionListener(this);
         buttonPanel.add(fitsButton);
         buttonPanel.add(doesnFitsButton);
+        buttonPanel.add(revertLastRuleButton);
 
         mainPanel.add(tilesPanel, BorderLayout.CENTER);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
         getContentPane().add(mainPanel);
-        pack();
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        RuleCreationGUI gui = new RuleCreationGUI();
+        try {
+            gui.updateTiles();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (NoMoreRulesException e) {
+
+        }
+
+    }
+
+    private void updateTiles() throws SQLException, NoMoreRulesException {
+        squareTopLeft.reset();
+        squareTopRight.reset();
+        squareDownRight.reset();
+        squareDownLeft.reset();
+
+        currentRule = dbInteractions.getNewRule();
+        switch (currentRule.next_to()) {
+            case 1:
+                squareTopLeft.fillWithImage(currentRule.this_TileFilepath());
+                squareDownLeft.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 2:
+                squareDownLeft.fillWithImage(currentRule.this_TileFilepath());
+                squareTopLeft.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 3:
+                squareTopLeft.fillWithImage(currentRule.this_TileFilepath());
+                squareTopRight.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 4:
+                squareTopRight.fillWithImage(currentRule.this_TileFilepath());
+                squareTopLeft.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 5:
+                squareTopLeft.fillWithImage(currentRule.this_TileFilepath());
+                squareDownRight.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 6:
+                squareTopRight.fillWithImage(currentRule.this_TileFilepath());
+                squareDownLeft.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 7:
+                squareDownRight.fillWithImage(currentRule.this_TileFilepath());
+                squareTopRight.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            case 8:
+                squareDownRight.fillWithImage(currentRule.this_TileFilepath());
+                squareTopLeft.fillWithImage(currentRule.that_TileFilepath());
+                break;
+
+            default:
+                throw new RuntimeException("no Direction found for direction id: " + currentRule.next_to());
+
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
-        boolean canItBe = false;
-        if (e.getSource() == fitsButton) {
-            canItBe = true;
+        if (e.getSource() == revertLastRuleButton) {
+            dbInteractions.revertRule(lastRule);
+        } else {
+            try {
+                if (e.getSource() == fitsButton || e.getSource() == doesnFitsButton) {
+                    lastRule = dbInteractions.putRuleIntoDB(currentRule.this_Tile(), currentRule.next_to(), currentRule.that_Tile(),
+                            e.getSource() == fitsButton);
+
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
+
         try {
-            dbInteractions.putRuleIntoDB(currentRule.this_Tile(), currentRule.next_to(), currentRule.that_Tile(), canItBe);
-            currentRule = dbInteractions.getNewRule();
-            getContentPane().removeAll();
-            displayRule();
-            revalidate();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, "Error connecting to database: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            updateTiles();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        } catch (NoMoreRulesException exception) {
+            System.out.println("no more rules left, you're done!");
+        }
+
+    }
+}
+
+class Square extends JPanel {
+    private BufferedImage image;
+
+    public Square() {
+
+    }
+
+    public void fillWithImage(String filepath) {
+        try {
+            image = ImageIO.read(new File(filepath));
+            repaint();
+        } catch (IOException ex) {
+            System.out.println("Error loading image from file: " + filepath);
+            ex.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws SQLException {
-        RuleCreationGUI gui = new RuleCreationGUI();
+    public void reset() {
+        image = null;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (image != null) {
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+
+    public Dimension getPreferredSize() {
+        return new Dimension(100, 100);
     }
 }
 
