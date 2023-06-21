92fd531239381b62be6d62522f8e2e01c27d823d
laurenz <i22032@hb.dhbw-stuttgart.de>
2023-06-21 12:39:02 +0200

    horrible additions to the controller absolute filepaths

--- src/main/java/de/schnittie/controller/Controller.java
+++ src/main/java/de/schnittie/controller/Controller.java
@@ -13,17 +13,23 @@ public class Controller {
         model.addListener(view);
     }
     public static void main(String[] args) {
+        //uncomment this to run normally
         Controller controller = new Controller();
+
+        //uncomment this to install and generate Rules
 //        Model model = new Model();
-//        if (true) {
+
+//          //Uncomment this if your Name starts with L
 //            model.addTiles("C:\\Users\\laure\\Desktop\\TilesTestingSockets\\Tilemap.png", 3);
 //            model.addTiles("C:\\Users\\laure\\Desktop\\TilesTestingSockets\\TilemapNonRotate.png", 0);
 //            model.addTiles("C:\\Users\\laure\\Desktop\\TilesTestingSockets\\6Brown0Green3Blue.png", 1);
+//
+            //uncomment this if your Name starts with E
 //            model.addTiles("C:\Users\eddi3\OneDrive\Dokumente\Uni\FemboyTileMaps\TilesTestingSockets\\Tilemap.png", 3);
 //            model.addTiles("C:\Users\eddi3\OneDrive\Dokumente\Uni\FemboyTileMaps\TilesTestingSockets\\TilemapNonRotate.png", 0);
 //            model.addTiles("C:\Users\eddi3\OneDrive\Dokumente\Uni\FemboyTileMaps\TilesTestingSockets\\6Brown0Green3Blue.png", 1);
 //            model.generateRules();
-//        }
+//
 //        model.generateMap(100,100);
 
 
