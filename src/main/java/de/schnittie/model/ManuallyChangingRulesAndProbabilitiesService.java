56751e658a0ef789de5f45245ea0258c04f86da6
laurenz <i22032@hb.dhbw-stuttgart.de>
2023-07-02 14:03:44 +0200

    ManuallyChanging...Sercive WIP

---
+++ src/main/java/de/schnittie/model/ManuallyChangingRulesAndProbabilitiesService.java
@@ -0,0 +1,17 @@
+package de.schnittie.model;
+
+import de.schnittie.model.database.DBinteractions;
+
+public class ManuallyChangingRulesAndProbabilitiesService {
+    public static DBinteractions dBinteractions = DBinteractions.getInstance();
+
+    public void removeRule(int ruleID){
+
+    }
+    public void addRule(int ruleID){
+
+    }
+    public void changeProbability(int ruleID, int probability){
+
+    }
+}
