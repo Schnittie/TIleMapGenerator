7a44fff0e23d638ef8158493558ab6383779b87c
laurenz <i22032@hb.dhbw-stuttgart.de>
2023-07-09 00:15:03 +0200

    ran some tests, wrote up some data

--- src/main/java/de/schnittie/Main.java
+++ src/main/java/de/schnittie/Main.java
@@ -1,6 +1,7 @@
 package de.schnittie;
 
 import de.schnittie.controller.Controller;
+import de.schnittie.model.ConfigurationLoaderService;
 import de.schnittie.model.businesscode.Configuration;
 import de.schnittie.model.database.InstallationHandler;
 
@@ -9,10 +10,21 @@ public class Main {
         InstallationHandler.generateTilesForDefaultMapIfNotPresent();
         Configuration.reloadConfiguration();
 
-        //ConfigurationLoaderService.loadConfiguration("C:\\Users\\laure\\AppData\\Local\\CatboyMaps\\TileMapGenerator\\FantasyConfig");
+        ConfigurationLoaderService.loadConfiguration("C:\\Users\\laure\\AppData\\Local\\CatboyMaps\\TileMapGenerator\\FantasyConfig");
         // 500x500 FantasyMap in 427118 Milliseconds
         // 500x500 FantasyMap in 461686 Milliseconds
         // 500x500 FantasyMap in 469251 Milliseconds
+
+        //with 100 sized Subboards
+        // 500x500 FantasyMap in 313359 Milliseconds
+
+        //with 150 sized Subboards
+        // 500x500 FantasyMap in 295664 Milliseconds
+
+        //with 200 sized Subboards
+        // 500x500 FantasyMap in 302465 Milliseconds
+
+
         Controller controller = new Controller();
     }
 }
