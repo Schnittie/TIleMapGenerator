fbedfe5bf4cd78481882530948171d0b4163bd53
jjongcafes <3ddiewolf@gmail.com>
2023-07-09 14:55:10 +0200

    probability Cell is now changable by user

--- src/main/java/de/schnittie/view/ProbabilityChangeFrame.java
+++ src/main/java/de/schnittie/view/ProbabilityChangeFrame.java
@@ -1,8 +1,14 @@
 package de.schnittie.view;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +27,12 @@ public class ProbabilityChangeFrame extends JDialog {
         HashMap<Integer, BufferedImage> iDtoImage = iDtoProbability.getTileIdToBufferedImageMap();
 
         String[] columnNames = {"Tile", "Probability"};
-        DefaultTableModel probabilityTableModel = new DefaultTableModel(columnNames, 0);
+        DefaultTableModel probabilityTableModel = new DefaultTableModel(columnNames, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column == 1;
+            }
+        };
 
         for (Map.Entry<Integer, Integer> entry : iDToProbabilityMap.entrySet()){
             Integer key = entry.getKey();
@@ -34,7 +45,74 @@ public class ProbabilityChangeFrame extends JDialog {
         int rowHeight = 40;
         probabilityTable.setRowHeight(rowHeight);
         probabilityTable.getColumnModel().getColumn(0).setCellRenderer(new ImageRenderer());
-        getContentPane().add(new JScrollPane(probabilityTable));
+
+        JLabel infoText = new JLabel("Input new probability");
+        infoText.setHorizontalAlignment(SwingConstants.CENTER);
+        infoText.setFont(infoText.getFont().deriveFont(Font.BOLD,20f));
+
+        probabilityTable.getColumnModel().getColumn(1).setCellRenderer(new EditableCellRenderer());
+        probabilityTable.getColumnModel().getColumn(1).setCellEditor(new EditableCellEditor());
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(infoText, BorderLayout.NORTH);
+        panel.add(new JScrollPane(probabilityTable), BorderLayout.CENTER);
+
+        getContentPane().add(panel);
+
+        probabilityTableModel.addTableModelListener(e -> {
+            if (e.getType() == TableModelEvent.UPDATE) {
+                int row = e.getFirstRow();
+                int column = e.getColumn();
+                if (column == 1) {
+                    Integer id = (Integer) probabilityTable.getValueAt(row, 0);
+                    Integer newProbability = (Integer) probabilityTable.getValueAt(row, column);
+                    iDToProbabilityMap.put(id, newProbability);
+                }
+            }
+        });
 
     }
+
+    private static class EditableCellRenderer extends JTextField implements TableCellRenderer{
+        public EditableCellRenderer() {
+            setOpaque(true);
+            setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+            setText(value != null ? value.toString() : "");
+            setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+            setForeground(isSelected ? table.getSelectionForeground() : table.getForeground());
+            return this;
+        }
+    }
+
+    private static class EditableCellEditor extends AbstractCellEditor implements TableCellEditor {
+        private JTextField editorComponent;
+
+        public EditableCellEditor() {
+            editorComponent = new JTextField();
+            editorComponent.setBorder(BorderFactory.createLineBorder(Color.BLUE)); // Set border color
+            //enter key stops edit
+            editorComponent.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    stopCellEditing();
+                }
+            });
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+            return editorComponent.getText();
+        }
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+            editorComponent.setText(value != null ? value.toString() : "");
+            return editorComponent;
+        }
+    }
+
 }
