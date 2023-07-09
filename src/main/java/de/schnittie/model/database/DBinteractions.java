b0c3dd17e21bc319abf185ca5d4657e8eba14c70
laurenz <i22032@hb.dhbw-stuttgart.de>
2023-07-09 10:37:24 +0200

    made adding rules compatible with very large rule sets

--- src/main/java/de/schnittie/model/database/DBinteractions.java
+++ src/main/java/de/schnittie/model/database/DBinteractions.java
@@ -29,6 +29,7 @@ public class DBinteractions {
 
     private Connection conn;
     private String dbFolder = DBinteractions.DefaultDB_FOLDER;
+    private static final int MAX_STATEMENT_SIZE = 40000;
 
     private static final DBinteractions dBInteractions;
 
@@ -248,6 +249,13 @@ public class DBinteractions {
     }
 
     public void putListOfRulesIntoDB(List<RuleTO> rules) {
+        if (rules.size() > MAX_STATEMENT_SIZE+1) {
+            List<RuleTO> firstRulesSlice =  rules.subList(0, MAX_STATEMENT_SIZE);
+            List<RuleTO> secondRulesSlice =  rules.subList(MAX_STATEMENT_SIZE, rules.size());
+            putListOfRulesIntoDB(firstRulesSlice);
+            putListOfRulesIntoDB(secondRulesSlice);
+            return;
+        }
         PreparedStatement statement = null;
         try {
             statement = conn.prepareStatement(
