7e8312a88a397c1ace6a635c66840b8603ca5be1
jjongcafes <3ddiewolf@gmail.com>
2023-07-10 20:17:27 +0200

    zoom and scroll and only one damn map shows up, so it works actually (zoom in lmb, zoom out rmb)

--- src/main/java/de/schnittie/view/ImagePanel.java
+++ src/main/java/de/schnittie/view/ImagePanel.java
@@ -2,14 +2,32 @@ package de.schnittie.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 class ImagePanel extends JPanel {
     private BufferedImage image;
+    private double scaleFactor = 1.0;
 
     public ImagePanel(BufferedImage image) {
         this.image = image;
-        setPreferredSize(new Dimension(600, 600));
+        setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+        setMinimumSize(new Dimension(image.getWidth(), image.getHeight()));
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    scaleFactor *= 1.1;
+                } else if (e.getButton() == MouseEvent.BUTTON3) {
+                    scaleFactor *= 0.9;
+                }
+                revalidate();
+                repaint();
+            }
+        });
+
     }
 
     public ImagePanel() {
@@ -19,11 +37,23 @@ class ImagePanel extends JPanel {
 
     public void setImage(BufferedImage image) {
         this.image = image;
+        setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+        setMinimumSize(new Dimension(image.getWidth(), image.getHeight()));
+        scaleFactor = 1.0;
+        revalidate();
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
+
+        int scaledWidth = (int) (image.getWidth() * scaleFactor);
+        int scaledHeight = (int) (image.getHeight() * scaleFactor);
+
+        int x = (getWidth() - scaledWidth) / 2;
+        int y = (getHeight() - scaledHeight) / 2;
+
+        g.drawImage(image, x, y, scaledWidth, scaledHeight, null);
     }
 }
