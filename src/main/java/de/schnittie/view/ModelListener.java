cb6dfa964bf2cfdab8534b0e6bff0b81b7c89fb8
jjongcafes <3ddiewolf@gmail.com>
2023-07-10 19:15:25 +0200

    put Controller, FrontendListener and Main into MapFrontend to get rid of unnecessary code

--- src/main/java/de/schnittie/view/ModelListener.java
+++ src/main/java/de/schnittie/view/ModelListener.java
@@ -2,6 +2,10 @@ package de.schnittie.view;
 
 import de.schnittie.model.mvcStuffs.MapGeneratorEvent;
 
+import java.awt.event.ActionEvent;
+
 public interface ModelListener {
+    void actionPerformed(ActionEvent e);
+
     void update(MapGeneratorEvent event);
 }
