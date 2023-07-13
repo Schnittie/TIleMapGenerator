6c1f9b205433575a818d33726fa0eb056ddb97ad
jjongcafes <3ddiewolf@gmail.com>
2023-07-13 18:28:18 +0200

    probability change with save button

--- src/main/java/de/schnittie/view/ProbabilityChangeFrame.java
+++ src/main/java/de/schnittie/view/ProbabilityChangeFrame.java
@@ -17,20 +17,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 import de.schnittie.model.GettingInformationService;
-import de.schnittie.view.ImageRenderer;
+
+import static de.schnittie.model.ManuallyChangingRulesAndProbabilitiesService.changeProbability;
 
 public class ProbabilityChangeFrame extends JDialog {
+
+    private DefaultTableModel probabilityTableModel;
+    private HashMap<Integer, Integer> iDToProbabilityMap;
+    private HashMap<Integer, BufferedImage> iDtoImage;
+    private JButton saveButton;
 
     public ProbabilityChangeFrame() {
         super();
         setSize(600, 600);
         setLocationRelativeTo(null);
         GettingInformationService iDtoProbability = new GettingInformationService();
-        HashMap<Integer,Integer> iDToProbabilityMap = iDtoProbability.getTileIdToProbabilityMap();
-        HashMap<Integer, BufferedImage> iDtoImage = iDtoProbability.getTileIdToBufferedImageMap();
+        iDToProbabilityMap = iDtoProbability.getTileIdToProbabilityMap();
+        iDtoImage = iDtoProbability.getTileIdToBufferedImageMap();
 
         String[] columnNames = {"Tile", "Probability"};
-        DefaultTableModel probabilityTableModel = new DefaultTableModel(columnNames, 0) {
+        probabilityTableModel = new DefaultTableModel(columnNames, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
                 return column == 1;
@@ -53,27 +59,37 @@ public class ProbabilityChangeFrame extends JDialog {
         infoText.setHorizontalAlignment(SwingConstants.CENTER);
         infoText.setFont(infoText.getFont().deriveFont(Font.BOLD,20f));
 
+        saveButton = new JButton("Save changes");
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveChanges();
+            }
+        });
+
         probabilityTable.getColumnModel().getColumn(1).setCellRenderer(new EditableCellRenderer());
         probabilityTable.getColumnModel().getColumn(1).setCellEditor(new EditableCellEditor());
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(infoText, BorderLayout.NORTH);
+        panel.add(saveButton, BorderLayout.SOUTH);
         panel.add(new JScrollPane(probabilityTable), BorderLayout.CENTER);
 
         getContentPane().add(panel);
 
-        probabilityTableModel.addTableModelListener(e -> {
-            if (e.getType() == TableModelEvent.UPDATE) {
-                int row = e.getFirstRow();
-                int column = e.getColumn();
-                if (column == 1) {
-                    BufferedImage image = (BufferedImage) probabilityTable.getValueAt(row,0);
-                    Integer id = getKeyByValue(image, iDtoImage);
-                    Integer newProbability = (Integer) probabilityTable.getValueAt(row, column);
-                    iDToProbabilityMap.put(id, newProbability);
-                }
-            }
-        });
+    }
+
+    private void saveChanges() {
+        for (int row = 0; row < probabilityTableModel.getRowCount(); row++) {
+            BufferedImage image = (BufferedImage) probabilityTableModel.getValueAt(row, 0);
+            Integer id = getKeyByValue(image, iDtoImage);
+            Integer newProbability = (Integer) probabilityTableModel.getValueAt(row, 1);
+            changeProbability(id, newProbability);
+        }
+
+/*
+        dispose();
+*/
     }
 
     private static Integer getKeyByValue(BufferedImage image, HashMap<Integer, BufferedImage> iDToImage){
@@ -105,7 +121,7 @@ public class ProbabilityChangeFrame extends JDialog {
 
         public EditableCellEditor() {
             editorComponent = new JTextField();
-            editorComponent.setBorder(BorderFactory.createLineBorder(Color.BLUE)); // Set border color
+            editorComponent.setBorder(BorderFactory.createLineBorder(Color.BLUE));
             //enter key stops edit
             editorComponent.addActionListener(new ActionListener() {
                 @Override
